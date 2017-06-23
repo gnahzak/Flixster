2f588d6de417e66b0227b40342220ddeabd1e836
gnahzak <kazhang@fb.com>
2017-06-22 17:56:43 -0700

    Butterknife improved adapters

--- app/src/main/java/com/example/kazhang/flicks/MovieAdapter.java
+++ app/src/main/java/com/example/kazhang/flicks/MovieAdapter.java
@@ -3,6 +3,7 @@ package com.example.kazhang.flicks;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,8 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
@@ -108,23 +111,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     // create viewholder as a static inner class
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-//        @BindView(R.id.ivPosterImage) ImageView ivPosterImage;
-//        @Nullable
-//        @BindView(R.id.ivBackdropImage) ImageView ivBackdropImage;
-         ImageView ivPosterImage;
-         ImageView ivBackdropImage;
-        TextView tvTitle;
-        TextView tvOverview;
+        @Nullable @BindView(R.id.ivPosterImage) ImageView ivPosterImage;
+        @Nullable @BindView(R.id.ivBackdropImage) ImageView ivBackdropImage;
+        @BindView(R.id.tvOverview) TextView tvOverview;
+        @BindView(R.id.tvTitle) TextView tvTitle;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            //ButterKnife.bind(this, itemView);
+            ButterKnife.bind(this, itemView);
 
             // look up view objects by ID
-             ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
-             ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
-            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
-            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
             itemView.setOnClickListener(this);
         }
 
